3cfaff8f41689755a6a082d1ae0b2ea376679f7e
Samuel Hoyas <samuelix97@gmail.com>
2025-12-16 16:08:09 +0100

    Basic styles configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import browserRouter from '@routes/index.tsx';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import './_main.scss';
 
 createRoot(document.getElementById('root')!).render(
   <RouterProvider router={browserRouter} />,
